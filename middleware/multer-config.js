45170a495712d5f9f4ed584239369433566dc2d8
Gerald Navarette <gerald.navarette@gmail.com>
2021-07-27 00:04:17 +0200

    improve image files naming

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -12,7 +12,8 @@ const storage = multer.diskStorage({
     callback(null, 'images');
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
+    const nameComplete = file.originalname.split(' ').join('_');
+    const name = nameComplete.split('.')[0];
     const extension = MIME_TYPES[file.mimetype];
     callback(null, name + Date.now() + '.' + extension);
   }
